93bbb286b9ba85d20f7ca18b3c3d0b18a73978a4
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2025-01-20 16:03:32 +0530

    Add files via upload

---
+++ Week1/Day1/LearnDatatypes.java
@@ -0,0 +1,40 @@
+package week1.day1;
+
+public class LearnDatatypes {
+	
+	public static void main(String[] args) {
+		
+		//byte
+		byte ageOfPerson=31;
+		
+		//short
+		short depositAmount=30000;
+		
+		//int
+		int deposit=50000;
+		
+		//long
+		long mobileNumber=8526511170l;
+		
+		//float - decimal
+		float rateOfInterest=9.3F;
+	
+		float amount1=0.0115552441256f;
+		System.out.println(amount1);
+		
+		double amount2=0.0115552441256;
+		System.out.println(amount2);
+		
+		//Char - store a single character as well as the Ascii value
+		char logoOfTestleaf='F';
+		char asciiValue=65;
+		System.out.println(asciiValue);
+		
+		//Boolean
+		boolean doYouKnowJava=true;
+		
+		//String
+		String name="Vineeth";
+	}
+
+}
